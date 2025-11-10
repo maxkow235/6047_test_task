85600641661e0aad58e238a514a17c567f34f794
Maksym Kovalskyi <maxkow235@gmail.com>
2025-11-10 07:20:42 +0200

    fixed deploy

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,21 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
+
+const isProd = process.env.NODE_ENV === 'production';
+const prefix = isProd ? '/6047_test_task' : '';
 
 const nextConfig: NextConfig = {
+  reactStrictMode: true,
+
+  basePath: prefix,
+  assetPrefix: prefix ? `${prefix}/` : undefined,
+
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-      },
-    ],
+    unoptimized: isProd,
+    path: `${prefix}/_next/image`,
   },
-  turbopack: {
-    root: process.cwd(),
-  },
+
+  output: 'export',
+  trailingSlash: true,
 };
 
 export default nextConfig;
